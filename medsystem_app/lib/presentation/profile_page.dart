f702a808b686505bf177d907c0d597eac1229225
alumnos <u202112353@upc.edu.pe>
2024-10-25 17:54:41 -0500

    feat: profile design updated

--- medsystem_app/lib/presentation/profile_page.dart
+++ medsystem_app/lib/presentation/profile_page.dart
@@ -6,37 +6,79 @@ class UserProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Perfil del Usuario'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Cambia esto por la URL de la imagen del usuario
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Nombre del Usuario',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Text(
-              'correo@ejemplo.com',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Imagen de fondo
+          Image.asset(
+            'assets/images/fondo.jpg', // Cambia esto por la ruta de tu imagen en assets
+            fit: BoxFit.cover,
+          ),
+          // Capa de sombra
+          Container(
+            color: const Color.fromARGB(255, 17, 18, 20).withOpacity(0.5), // Sombra sobre la imagen
+          ),
+          // Contenido
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
                 
-              },
-              child: const Text('Editar Perfil'),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Cambia esto por la URL de la imagen del usuario
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      'Nombre del Usuario',
+                      style: TextStyle(fontSize: 24,
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'correo@ejemplo.com',
+                      style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                    const Text(
+                      'dni:12345678',
+                      style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 255, 255, 255)),
+                    ),
+                    const SizedBox(height: 32),
+                    OutlinedButton(
+                      onPressed: () {
+                        // Acción del botón
+                      },
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Colors.white), // Borde blanco
+                        backgroundColor: Colors.transparent, // Fondo transparente
+                      ),
+                      child: const Text(
+                        'Regresar',
+                        style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)), // Texto blanco
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          // AppBar en la parte superior
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              title: const Text('Perfil del Usuario'),
+              backgroundColor: const Color.fromARGB(0, 244, 242, 242), // Hacer el AppBar transparente
+              elevation: 0, // Sin sombra
+            ),
+          ),
+        ],
       ),
     );
   }
