7a506ea3ba9b182d077cab536e87eab487d43f88
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-10-25 11:12:45 -0500

    fix: update position elements to add treatments page

--- medsystem_app/lib/presentation/treatments/add_treatments_page.dart
+++ medsystem_app/lib/presentation/treatments/add_treatments_page.dart
@@ -20,13 +20,15 @@ class _AddTreatmentsScreenState extends State<AddTreatmentsScreen> {
         backgroundColor: const Color.fromRGBO(46, 63, 110, 1),
       ),
       backgroundColor: const Color.fromRGBO(46, 63, 110, 1),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Column(
-              children: [
-                ElevatedButton(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: ElevatedButton(
                   onPressed: () {
                     Navigator.pushReplacement(
                       context,
@@ -37,79 +39,84 @@ class _AddTreatmentsScreenState extends State<AddTreatmentsScreen> {
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromRGBO(46, 63, 110, 1),
-                    padding: const EdgeInsets.fromLTRB(30, 5, 5, 5),
+                    padding: const EdgeInsets.all(5),
                     minimumSize: const Size(20, 30),
                   ),
                   child: const Icon(Icons.arrow_back, color: Colors.white),
                 ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: Card(
-                child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Add Treatments',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20),
-                        ),
-                        Form(
-                          child: Column(
-                            children: <Widget>[
-                              TextFormField(
-                                  decoration: const InputDecoration(
-                                      labelText: 'Name of Patients'),
-                                  validator: (String? value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter some text';
-                                    }
-                                    return null;
-                                  }),
-                              TextFormField(
-                                  decoration: const InputDecoration(
-                                      labelText: 'Name of Treatments'),
-                                  validator: (String? value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter some text';
-                                    }
-                                    return null;
-                                  }),
-                              TextFormField(
-                                  decoration: const InputDecoration(
-                                      labelText: 'Description'),
-                                  validator: (String? value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter some text';
-                                    }
-                                    return null;
-                                  }),
-                              TextFormField(
-                                  decoration: const InputDecoration(
-                                      labelText: 'Period'),
-                                  validator: (String? value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter some text';
-                                    }
-                                    return null;
-                                  }),
-                              const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 16.0),
-                                child: ElevatedButton(
-                                    onPressed: null, child: Text("Submit")),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    )),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Add Treatments',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Form(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Name of Patients'),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Name of Treatments'),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Description'),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Period'),
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 16.0),
+                              child: ElevatedButton(
+                                onPressed: null,
+                                child: Text("Submit"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
