654ef236cc79f6e235440de1179ed486138f4f95
JuanMontes18 <u202210775@upc.edu.pe>
2024-10-25 19:38:10 -0500

    feat: implementation of the main page to the main

--- medsystem_app/lib/main.dart
+++ medsystem_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medsystem_app/appointment_page.dart';
+import 'package:medsystem_app/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Prueba'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false, 
+      //home: HomePage(),
+      home: AppointmentPage()
     );
   }
 }
