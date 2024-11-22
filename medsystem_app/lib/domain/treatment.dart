4333ee187e8baa999205d7f44f893264edff16c1
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-11-22 01:59:29 -0500

    fix: modify some attributes in treatment class

--- medsystem_app/lib/domain/treatment.dart
+++ medsystem_app/lib/domain/treatment.dart
@@ -2,19 +2,39 @@ class Treatment {
   int id;
   String treatmentName;
   String description;
-  String period;
+  String startDate;
+  String endDate;
   int patientId;
 
   Treatment(
       {required this.id,
       required this.treatmentName,
       required this.description,
-      required this.period,
+      required this.startDate,
+      required this.endDate,
       required this.patientId});
-  Treatment.fromJson(Map<String, dynamic> json)
-      : id = json['id'] ?? -1,
-        treatmentName = json['treatmentName'] ?? '',
-        description = json['description'] ?? '',
-        period = json['period'] ?? '',
-        patientId = json['patientId'] ?? -1;
+
+  String get period => '$startDate - $endDate';
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'treatmentName': treatmentName,
+      'description': description,
+      'startDate': startDate,
+      'endDate': endDate,
+      'patientId': patientId,
+    };
+  }
+
+  factory Treatment.fromJson(Map<String, dynamic> json) {
+    return Treatment(
+      id: json['id'] ?? -1,
+      treatmentName: json['treatmentName'] ?? '',
+      description: json['description'] ?? '',
+      startDate: json['startDate'] ?? '',
+      endDate: json['endDate'] ?? '',
+      patientId: json['patientId'] ?? -1,
+    );
+  }
 }
