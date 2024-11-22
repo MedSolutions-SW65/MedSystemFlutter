df64aab2beaa82e9d7d59f1b240579f499e99dfe
ManuGZ <galonso097@gmail.com>
2024-11-22 07:27:32 -0500

    feat: profile updated to fetch data of doctor or patient

--- medsystem_app/lib/features/auth/presentation/pages/profile_page.dart
+++ medsystem_app/lib/features/auth/presentation/pages/profile_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:medsystem_app/features/auth/data/remote/auth_service.dart';
 
 class UserProfilePage extends StatefulWidget {
@@ -9,6 +12,7 @@ class UserProfilePage extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfilePage> {
+  String? fullName;
   String? userEmail;
 
   @override
@@ -22,8 +26,50 @@ class _UserProfilePageState extends State<UserProfilePage> {
     authService.signOut();
   }
 
-  void loadUser() async {
+  Future<void> loadUser() async {
     final authService = AuthService();
+    final user = await authService.getCurrentUser();
+    final uid = user?.uid;
+
+    if (uid == null) {
+      setState(() {
+        fullName = "Usuario desconocido";
+      });
+      return;
+    }
+
+    try {
+      // Consulta en doctors
+      final doctorResponse = await http
+          .get(Uri.parse('http://10.0.2.2:8080/api/v1/doctors/uid/$uid'));
+      if (doctorResponse.statusCode == 200) {
+        final doctorData = json.decode(doctorResponse.body);
+        setState(() {
+          fullName = doctorData['fullName'];
+        });
+        return;
+      }
+
+      // Consulta en patients si no está en doctors
+      final patientResponse = await http
+          .get(Uri.parse('http://10.0.2.2:8080/api/v1/patients/uid/$uid'));
+      if (patientResponse.statusCode == 200) {
+        final patientData = json.decode(patientResponse.body);
+        setState(() {
+          fullName = patientData['fullName'];
+        });
+        return;
+      }
+
+      setState(() {
+        fullName = "Usuario no encontrado";
+      });
+    } catch (e) {
+      setState(() {
+        fullName = "Error al cargar datos";
+      });
+    }
+
     final email = await authService.getCurrentUserEmail();
     setState(() {
       userEmail = email;
@@ -36,14 +82,14 @@ class _UserProfilePageState extends State<UserProfilePage> {
       body: Stack(
         fit: StackFit.expand,
         children: [
-          // Background Image
+          // Imagen de fondo
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: const AssetImage("assets/images/fondo.jpg"),
                 fit: BoxFit.cover,
                 colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5), // Cambia la opacidad aquí
+                  Colors.black.withOpacity(0.5),
                   BlendMode.darken,
                 ),
               ),
@@ -76,9 +122,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
                           "assets/images/profile.jpg"), // Cambia esto por la URL de la imagen del usuario
                     ),
                     const SizedBox(height: 16),
-                    const Text(
-                      'Nombre del Usuario',
-                      style: TextStyle(
+                    Text(
+                      'Hola de nuevo ${fullName ?? 'Cargando...'}',
+                      style: const TextStyle(
                         fontSize: 24,
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
@@ -88,14 +134,6 @@ class _UserProfilePageState extends State<UserProfilePage> {
                     Text(
                       userEmail ?? 'Cargando...',
                       style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      'DNI: 12345678',
-                      style: TextStyle(
                         fontSize: 16,
                         color: Colors.black,
                       ),
