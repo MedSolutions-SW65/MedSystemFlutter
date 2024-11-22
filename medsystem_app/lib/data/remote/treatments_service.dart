0f34e4e5a726abd9d623a3f65e2f11beaeb3a06b
Astrid Boronda Heidinger <108700549+AstridBH@users.noreply.github.com>
2024-11-22 02:00:19 -0500

    feat: add AddTreatmentService class

--- medsystem_app/lib/data/remote/treatments_service.dart
+++ medsystem_app/lib/data/remote/treatments_service.dart
@@ -4,8 +4,9 @@ import 'dart:io';
 import 'package:medsystem_app/domain/treatment.dart';
 import 'package:http/http.dart' as http;
 
+const baseUrl = 'http://10.0.2.2:8080/api/v1/treatments';
+
 class TreatmentsService {
-  final baseUrl = 'http://10.0.2.2:8080/api/v1/treatments';
   Future<List<Treatment>> getTreatments() async {
     Uri uri = Uri.parse(baseUrl);
     http.Response response = await http.get(uri);
@@ -14,5 +15,29 @@ class TreatmentsService {
       return maps.map((json) => Treatment.fromJson(json)).toList();
     }
     return [];
+  }
+}
+
+class AddTreatmentService {
+  Future<Treatment?> postTreatments(String treatmentName, String description,
+      String startDate, String endDate, String patientId) async {
+    http.Response response = await http.post(
+      Uri.parse(baseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(Treatment(
+        id: 0,
+        treatmentName: treatmentName,
+        description: description,
+        startDate: startDate,
+        endDate: endDate,
+        patientId: int.parse(patientId),
+      ).toMap()),
+    );
+    if (response.statusCode == HttpStatus.ok) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      final treatment = Treatment.fromJson(json);
+      return treatment;
+    }
+    return null;
   }
 }
